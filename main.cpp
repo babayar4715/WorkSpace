b0b6ad74b48cf79efbc8a89ef4640aeadeb9eb45
Babaiar <119310971+babayar4715@users.noreply.github.com>
2025-03-02 23:13:55 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,68 @@
+// Section 6
+// The size of operator 
+
+
 #include <iostream> 
+#include <climits> // make sure you include climits for integer types 
+                    // Similar information for floating point numbers 
+                    // is container in <cfloat>
+
 
 using namespace std;
 
 int main() {
-    cout << "Hello, Welcome Frank's carpet cleaning" << endl;
-    cout << "\nHow many small rooms do you want to clean?"<<endl;
-    int number_of_small_rooms {0};
-    cin >> number_of_small_rooms;
-    cout <<"\nHow many large rooms do you want to clean?"<<endl;
-    int number_of_large_rooms {0};
-    cin >> number_of_large_rooms; 
-    
-    const double price_per_small_room {25};
-    const double price_per_large_room {35};
-    const double sales_tax {0.06};
-    int estimate_expire {30};
-    double total_cost = (price_per_large_room * number_of_large_rooms) + (price_per_small_room * number_of_small_rooms);
-    
-    cout << "\nEstimate for carpet cleaning service" << endl;
-    cout << "\nNumber of small rooms: " << number_of_small_rooms<<endl;
-    cout << "Number of large rooms: " << number_of_large_rooms<<endl;
-    
-    cout << "Price per small room: $"<<price_per_small_room<<" ==> $" << price_per_small_room * number_of_small_rooms<<  endl;
-    cout << "Price per large room: $"<<price_per_large_room<<" ==> $" << price_per_large_room * number_of_large_rooms<<  endl;
-    
-    cout << "Cost: $" << total_cost <<endl;
-    cout << "Tax: $" << total_cost * sales_tax << endl;
-    
-    cout << "=============================================="<<endl;
-    cout << "Total estimate: $" <<(total_cost * sales_tax)+total_cost<< endl;
-    cout << "This extimate is valid for " << estimate_expire<< " days" <<endl;
     
     
+    cout << "size of information" << endl;
+    cout << "======================================"<<endl;
     
+    cout << "char: " << sizeof(char) << "bytes." << endl;
+    cout << "int: " << sizeof(int) << "bytes." <<endl;
+    cout << "unsigned int:" << sizeof(unsigned int) << "bytes." << endl;
+    cout << "short:" << sizeof(short)<< "bytes."<<endl;
+    cout << "long:" << sizeof(long) << "bytes." << endl;
+    cout << "long long:" << sizeof(long long) << "bytes." << endl;
     
+    cout << "===============================================" << endl;
+    
+    cout << "float:" << sizeof(float) << " bytes." << endl;
+    cout << "double:" << sizeof(double) << "bytes." << endl;
+    cout << "long double:" << sizeof(long double) << "bytes." << endl;
+    
+    // use values defined in <climits>
+    
+    cout << "=================================================================" << endl;
+    
+    cout << "Minimum values: " << endl;
+    cout << "char:" << CHAR_MIN << endl;
+    cout << "int:" << INT_MIN <<  endl;
+    cout << "short:" << SHRT_MIN << endl;
+    cout << "long:" << LONG_MIN << endl;
+    cout << "long long:" << LLONG_MIN << endl;
+    
+     cout << "=================================================================" << endl;
+    
+    cout << "Maximum values: " << endl;
+    cout << "char:" << CHAR_MAX << endl;
+    cout << "int:" << INT_MAX <<  endl;
+    cout << "short:" << SHRT_MAX << endl;
+    cout << "long:" << LONG_MAX << endl;
+    cout << "long long:" << LLONG_MAX << endl;
+    
+    // sizeof cal also be used with variable names;
+    cout << "=================================================================" << endl; 
+         
+    cout << "sizeof using variables names" <<endl;
+     int age {21};
+     cout<< "age is" << sizeof(age) << "bytes." << endl;
+     // or 
+     cout<< "age is" << sizeof age << "bytes." << endl;
+     
+     double wage {22.24};
+     cout<< "wage is" << sizeof(wage) << "bytes." << endl;
+     //or
+     cout<< "wage is" << sizeof wage << "bytes." << endl;
+     
     
     return 0;
     }
