6e3f3bc58c9cb8f8aa482c84bc882def4ff6a831
Babaiar <119310971+babayar4715@users.noreply.github.com>
2025-03-05 23:00:15 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,68 +1,43 @@
-// Section 6
-// The size of operator 
-
-
 #include <iostream> 
-#include <climits> // make sure you include climits for integer types 
-                    // Similar information for floating point numbers 
-                    // is container in <cfloat>
-
 
 using namespace std;
 
 int main() {
-    
-    
-    cout << "size of information" << endl;
-    cout << "======================================"<<endl;
-    
-    cout << "char: " << sizeof(char) << "bytes." << endl;
-    cout << "int: " << sizeof(int) << "bytes." <<endl;
-    cout << "unsigned int:" << sizeof(unsigned int) << "bytes." << endl;
-    cout << "short:" << sizeof(short)<< "bytes."<<endl;
-    cout << "long:" << sizeof(long) << "bytes." << endl;
-    cout << "long long:" << sizeof(long long) << "bytes." << endl;
-    
-    cout << "===============================================" << endl;
-    
-    cout << "float:" << sizeof(float) << " bytes." << endl;
-    cout << "double:" << sizeof(double) << "bytes." << endl;
-    cout << "long double:" << sizeof(long double) << "bytes." << endl;
-    
-    // use values defined in <climits>
-    
-    cout << "=================================================================" << endl;
-    
-    cout << "Minimum values: " << endl;
-    cout << "char:" << CHAR_MIN << endl;
-    cout << "int:" << INT_MIN <<  endl;
-    cout << "short:" << SHRT_MIN << endl;
-    cout << "long:" << LONG_MIN << endl;
-    cout << "long long:" << LLONG_MIN << endl;
-    
-     cout << "=================================================================" << endl;
-    
-    cout << "Maximum values: " << endl;
-    cout << "char:" << CHAR_MAX << endl;
-    cout << "int:" << INT_MAX <<  endl;
-    cout << "short:" << SHRT_MAX << endl;
-    cout << "long:" << LONG_MAX << endl;
-    cout << "long long:" << LLONG_MAX << endl;
-    
-    // sizeof cal also be used with variable names;
-    cout << "=================================================================" << endl; 
-         
-    cout << "sizeof using variables names" <<endl;
-     int age {21};
-     cout<< "age is" << sizeof(age) << "bytes." << endl;
-     // or 
-     cout<< "age is" << sizeof age << "bytes." << endl;
-     
-     double wage {22.24};
-     cout<< "wage is" << sizeof(wage) << "bytes." << endl;
-     //or
-     cout<< "wage is" << sizeof wage << "bytes." << endl;
-     
-    
+   char vowel [] {'a','b','c','d','e'};
+   cout << "\n The first vowel is "<< vowel[0] << endl; 
+   cout << "The last vowel is "<<vowel[4]<<endl;
+   
+   double high_temp [] {90.1, 89.5, 95.6, 96.3};
+   cout << "\n The first temperature is "<< high_temp[0]<<endl;
+   high_temp [0]= 100.7;
+   cout << "The first temperature is now "<<high_temp[0]<<endl;
+   
+   int test_score [] {1,2,3,4,5};
+   cout <<"\n First score at index 0: "<< test_score[0] <<endl;
+   cout <<"Second score at index 1: "<<test_score[1] <<endl;
+   cout <<"Third score at index 2: " << test_score[2] << endl;
+   cout <<"Forth score at index 3: " << test_score[3] << endl;
+   cout <<"Fifth score at index 4: " << test_score[4] << endl;
+   
+   cout << "\nEnter five scores"<<endl;
+   cin >> test_score [0];
+   cin >> test_score [1];
+   cin >> test_score [2];
+   cin >> test_score [3];
+   cin >> test_score [4];
+   
+   
+   cout << "\nNew updated scores are"<<endl;
+   cout << "First score in index 0: "<< test_score [0]<<endl;
+   cout << "Second score in index 1: "<< test_score [1]<<endl;
+   cout << "Third score in index 2: "<< test_score [2]<<endl;
+   cout << "Forth score in index 3: "<< test_score [3]<<endl;
+   cout << "Fifth score in index 4: "<< test_score [4]<<endl;
+
+
+
+   
+   
+   
     return 0;
     }
