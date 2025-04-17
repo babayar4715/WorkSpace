96a2324b91c1b00454771b617f78c1893e688e1d
Babaiar <119310971+babayar4715@users.noreply.github.com>
2025-04-16 22:58:52 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,76 +1,15 @@
 #include <iostream> 
 
-using namespace std;
-
-// increment operator ++
-// decrement operator -- 
-
 int main() {
-  
     
-    /*
-    int counter {10};
-    int result {0};
+    int num1 {10};
+    int num2 {20};
+    num1 = num2 = 1000;
     
+    std::cout << "First number: " << num1 << std::endl;
+    std::cout << "Second number: " << num2 << std::endl;
+
     
-    cout << "Counter: " << counter << endl;
-    
-    counter = counter + 1; 
-    cout << "Counter: " << counter << endl;
-    
-    counter ++;
-    cout << "Counter: " << counter << endl;
-    
-    ++counter;
-    cout << "Counter: " << counter << endl;
-    */
-    // increment
-    /*
-    int counter = 10;
-    int result = 0;
-    
-    cout << "Counter: " << counter << endl;
-    
-    result = ++counter;
-    cout << "Counter: " << counter << endl; >> there are counter was 11 
-    cout << "Result: " << result << endl;  >> while here is also 11  // but the counter were 10 initially
-    */
-    
-    // post increment
-    /*
-    int counter = 10;
-    int result = 0;
-    
-    cout << "Counter: " << counter << endl; // here is counter 10
-    
-    result = counter++;
-    cout << "Counter: " << counter << endl; // while here counter is became to 11
-    cout << "Result: " << result << endl; // but here it becomes result 10 again.
-    */
-    
-   /*    int counter = 10;
-    int result = 0;
-    
-    cout << "Counter: " << counter << endl; // here is counter 10
-    
-    result = ++counter + 10;
-    cout << "Counter: " << counter << endl; // while here counter is became to 11
-    cout << "Result: " << result << endl;
-   */
-   /*
-     int counter = 10;
-    int result = 0;
-    
-    cout << "Counter: " << counter << endl; // here is counter 10
-    
-    result = counter++ + 10;
-    cout << "Counter: " << counter << endl; // while here counter is became to 11
-    cout << "Result: " << result << endl;
-    
-    */
-    
-    
-    
-     
     return 0;
-    }
+    
+}
