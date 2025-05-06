133794d9a90a6623bc8fae6329ea8ad49c78375e
Babaiar <119310971+babayar4715@users.noreply.github.com>
2025-05-05 21:31:35 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,44 +3,28 @@
 using namespace std;
 
 int main() {
-   int num {};
-   const int min {10};
-   const int max {100};
-   
-   cout << "Enter a number between " << min << " and " << max << ": ";
-   cin >> num;
-   
-   if (num >= min) {
-       cout << "\n=================if statament 1 =================" << endl;
-       cout << num << " is greater than or equal to " << min << endl;
-       
-       int diff {num - min};
-       cout << num << " is " << diff << " greater than " << min << endl;
-   }
-
-    if (num <= max) {
-        cout << "\n=================if statament 2 =================" << endl;
-        cout << num << " is less that or equal to " << max << endl;
+    
+    int num{};
+    const int target {10};
+    
+    cout << "Enter a numbdr and I'll compare it to " << target << ": ";
+    cin >> num;
+    
+    if(num >= target){
+        cout << "\n================================================"<< endl;
+        cout << num << " is greater than or equal to " << target << endl;
         
-    int diff {max - num};
-    cout << num << " is " << diff << " less than " << max << endl;
-    
+        int diff {num - target};
+        cout << num << " is " << diff << " greater  than " << target << endl;
+        
+    }else{
+        cout << "\n=================================================" << endl;
+        cout << num << "is less than " << target << endl;
+        
+        int diff {target - num};
+        cout << num << " is " << diff << " less than " << target << endl;
+    }
+cout << endl;
 
-    }
-    
-    if (num >= min && num <= max) {
-        cout << "\n=================if statament 3 =================" << endl;
-        cout << num << " is in range "<< endl;
-        cout << "This means statements 1 and 2 must also display!! " << endl;
-    }
-    
-if (num == min || num == max){
-        cout << "\n=================if statament 4 =================" << endl;
-        cout << num << " is right on a boundary " << endl;
-        cout << "This means all 4 statements display" << endl;
-}
-    
-    
-    
     return 0;
     }
